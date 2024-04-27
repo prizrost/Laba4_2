163f2dbd743621ca07b1648438175348c99c261a
prizrost <prizrost0@gmail.com>
2024-04-27 14:10:04 +0900

    laba 4-2 with methods

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static Scanner sc;
+    public static int[] newArr() throws NegativeArraySizeException, InputMismatchException{
+        System.out.print("Введите сколько элементов хотите добавить: ");
+        int [] arr = new int[sc.nextInt()];
+        return arr;
+    }
+    public static int fillArr(int[] arr) throws InputMismatchException, RuntimeException{
+        int sum = 0;
+        int k = 0;
+        for(int i = 0; i < arr.length; i++){
+            System.out.print("Введите число " + (i + 1) + "-й элементу: ");
+            arr[i] = sc.nextInt();
+            if(arr[i] > 0){
+                sum += arr[i];
+                k++;
+            }
+        }
+        if(k==0){
+            throw new RuntimeException("Положительные элементы отстутсвуют");
+        }
+        return (sum / k);
+    }
+    public static void main(String[] args) {
+        try {
+            sc = new Scanner(System.in);
+            int[] arr = newArr();
+            System.out.println("Среднее значение: " + fillArr(arr));
+        }
+        catch (NegativeArraySizeException e){
+            System.out.println("Ввели отрицательное число размеру массива");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Ввод строки вместо числа");
+        }
+        catch (RuntimeException e){
+            System.out.println(e);
         }
     }
 }
